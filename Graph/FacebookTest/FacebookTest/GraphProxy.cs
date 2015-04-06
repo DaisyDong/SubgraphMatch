c28939dfb4f973f9119f5da3213a9ac9bdd20821
Daisy <1723767242@qq.com>
2015-04-06 19:10:09 +0800

    first complete

--- Graph/FacebookTest/FacebookTest/GraphProxy.cs
+++ Graph/FacebookTest/FacebookTest/GraphProxy.cs
@@ -15,6 +15,11 @@ namespace FacebookTest
     class GraphProxy : GraphProxyBase
     {
         public SameLabel sl = new SameLabel();
+        public int slave_count = Global.SlaveCount;
+        public ResultReader[][] rr = new ResultReader[2][];
+        public HashSet<long> leafSet = new HashSet<long>(); //保存每个匹配结果中的叶子节点 
+        public List<NodeSet> result = new List<NodeSet>();
+        public int STwigNum = 2;    //分好的STwig的个数
 
         public override void BeginProxyHandler(BeginSignalReader bsr)
         {
@@ -26,7 +31,7 @@ namespace FacebookTest
             Q[0].set.Add(5002);
             Q[0].set.Add(5005);
 
-            Q[1].headid = 5003;
+            Q[1].headid = 5002;
             Q[1].set.Add(5004);
             Q[1].set.Add(5005);
 
@@ -34,21 +39,22 @@ namespace FacebookTest
             sl.set = new List<NodeSet>();
             Console.WriteLine(slave_count);
             //Console.ReadLine();
-            ResultReader[] rr = new ResultReader[slave_count];
+            // ResultReader[][] rr = new ResultReader[2][slave_count];
             for (int q = 0; q < 2; q++)
             {
+                rr[q] = new ResultReader[slave_count];
                 for (int i = 0; i < slave_count; i++)
                 {
                     Global.CloudStorage.TellSlaveIDToGraphSlave(i, new SlaveIDWriter(i));
                     //Console.WriteLine("slave id : {0}", i);
                     Global.CloudStorage.GetSameLabelSetToGraphSlave(i, new SameLabelWriter(sl.set));
-                    Console.WriteLine("q{0} to slave{1}",q,i);
+                    Console.WriteLine("q{0} to slave{1}", q, i);
 
-                    rr[i] = Global.CloudStorage.QueryToGraphSlave(i, new NodeSetWriter(Q[q].headid, Q[q].headname, Q[q].headlabel, Q[q].set.ToList()));
+                    rr[q][i] = Global.CloudStorage.QueryToGraphSlave(i, new NodeSetWriter(Q[q].headid, Q[q].headname, Q[q].headlabel, Q[q].set.ToList()));
 
-
-                    rr[i].samelabel.ForEach((tmp) =>
+                    rr[q][i].samelabel.ForEach((tmp) =>
                     {
+                        Console.WriteLine("{0}", tmp.headid);
                         sl.set.Add(tmp);
                     });
                     Console.WriteLine("Done!!");
@@ -56,17 +62,17 @@ namespace FacebookTest
                     Console.WriteLine();
 
 
-                //    rr[i].set.ForEach((tmp1) =>
-                //{
-                //    Console.WriteLine(tmp1.headid);
-                //    tmp1.set.ForEach((tmp2) =>
-                //    {
-                //        Console.Write("{0} ", tmp2);
-                //    }
-                //    );
-                //    Console.WriteLine();
-                //}
-                //    );
+                    //    rr[i].set.ForEach((tmp1) =>
+                    //{
+                    //    Console.WriteLine(tmp1.headid);
+                    //    tmp1.set.ForEach((tmp2) =>
+                    //    {
+                    //        Console.Write("{0} ", tmp2);
+                    //    }
+                    //    );
+                    //    Console.WriteLine();
+                    //}
+                    //    );
 
 
 
@@ -76,36 +82,87 @@ namespace FacebookTest
                 }
             }
             Console.WriteLine("finished");
+            ReportHandler();
         }
         public override void UpdateSameLabelSetHandler(SameLabelReader slr)
         {
 
         }
-        public override void QueryHandler(NodeSetReader nsr, ResultWriter result)
+        /* public void QueryHandler(NodeSetReader nsr, ResultWriter result)
+         {
+             int slave_count = Global.SlaveCount;
+             sl.set = new List<NodeSet>();
+             Console.WriteLine(slave_count);
+             Console.ReadLine();
+             //ResultReader[] rr = new ResultReader[slave_count];
+             for (int i = 0; i < slave_count; i++)
+             {
+                 Global.CloudStorage.TellSlaveIDToGraphSlave(i, new SlaveIDWriter(i));
+                 Console.WriteLine("slave id : {0}", i);
+                 Global.CloudStorage.GetSameLabelSetToGraphSlave(i, new SameLabelWriter(sl.set));
+                 rr[i] = Global.CloudStorage.QueryToGraphSlave(i, new NodeSetWriter(nsr.headid, nsr.headname, nsr.headlabel, nsr.set));
+                 rr[i].samelabel.ForEach((tmp) =>
+                     {
+                         sl.set.Add(tmp);
+                     });
+             }
+             for (int i = 0; i < slave_count; i++)
+             {
+                 rr[i].set.ForEach((tmp) =>
+                 {
+                     result.set.Add(tmp);
+                 });
+             }
+          }*/
+        //处理每个slave的结果,rr[i][j] 表示第i个STwig在第j个机器中的结果
+        public void ReportHandler()
         {
-            int slave_count = Global.SlaveCount;
-            sl.set = new List<NodeSet>();
-            Console.WriteLine(slave_count);
-            Console.ReadLine();
-            ResultReader[] rr = new ResultReader[slave_count];
-            for (int i = 0; i < slave_count; i++)
-            {
-                Global.CloudStorage.TellSlaveIDToGraphSlave(i, new SlaveIDWriter(i));
-                Console.WriteLine("slave id : {0}", i);
-                Global.CloudStorage.GetSameLabelSetToGraphSlave(i, new SameLabelWriter(sl.set));
-                rr[i] = Global.CloudStorage.QueryToGraphSlave(i, new NodeSetWriter(nsr.headid, nsr.headname, nsr.headlabel, nsr.set));
-                rr[i].samelabel.ForEach((tmp) =>
+            for (int j = 0; j < Global.SlaveCount; j++)
+            {  
+                int n = 1;
+                rr[0][j].samelabel.ForEach((tmp) =>
                     {
-                        sl.set.Add(tmp);
+                        tmp.set.ForEach((num) =>
+                            { 
+                                leafSet.Add(num);
+                            });
+                        if (n == STwigNum)
+                        {
+                            Console.WriteLine("match subGraph is:");
+                            Console.WriteLine("{0}:{1}", tmp.headid, tmp.set);
+                        }
+                        else findResult(n,j); 
+                        //  Console.WriteLine("{0}", tmp.headid);
+                        //result.Add(tmp);
+                         
                     });
             }
-            for (int i = 0; i < slave_count; i++)
-            {
-                rr[i].set.ForEach((tmp) =>
-                {
-                    result.set.Add(tmp);
-                });
-            }
         }
+    public void findResult(int n,int m){
+        HashSet<long> leafSetS = leafSet;
+        rr[n][m].samelabel.ForEach((tem) =>
+            {
+                if (leafSetS.Contains(tem.headid))
+                { 
+                    tem.set.ForEach((num) =>
+                        { 
+                            if(num != tem.headid)
+                                leafSet.Add(num);
+                        });
+                    result.Add(tem);
+                    if (++n == STwigNum)
+                    {
+                        Console.WriteLine("match subgraph is:");
+                        result.ForEach((tmp) =>
+                            {
+                                Console.WriteLine("{0}:{1}", tmp.headid, tmp.set);
+                            });
+                    }
+                    else
+                        findResult(n, m);
+                }
+            });
+        return;
     }
+    }       
 }
---
+++ Graph/TrinityFacebook/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Trinity;
+using Trinity.Storage;
+using Trinity.Network.Messaging;
+
+namespace GraphTest
+{
+    class Graph
+    {
+        public static NodeSet[] Q = new NodeSet[2];
+        //public static object LockThis = new object();
+        //public static List<NodeSet> AllSameLabel = new List<NodeSet>();
+        public static long GraphSize = 10;
+
+
+
+
+
+        public void Initialize()
+        {
+            Q[0] = new NodeSet();
+            Q[1] = new NodeSet();
+            Q[0].headid = 102;
+            Q[0].set.Add(101);
+            Q[0].set.Add(102);
+            Q[0].set.Add(103);
+            Q[1].headid = 104;
+            Q[1].set.Add(103);
+            Q[1].set.Add(104);
+            Q[1].set.Add(101);
+        }
+    }
+}
+
